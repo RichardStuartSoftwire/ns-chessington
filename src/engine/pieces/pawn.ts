fd0d5b61ec54629cb56de4a11e917ec84ee200ef
Richard Stuart (Softwire) <richard.stuart@softwire.com>
2026-09-10 17:25:09 +0100

    pawns can move 2 squares forward on their own first move (still no available space checks

--- src/engine/pieces/pawn.ts
+++ src/engine/pieces/pawn.ts
@@ -2,6 +2,7 @@ import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
 import Square from '../square';
+import GameSettings from '../gameSettings';
 
 export default class Pawn extends Piece {
     public constructor(player: Player) {
@@ -10,10 +11,22 @@ export default class Pawn extends Piece {
 
     public getAvailableMoves(board: Board) {
         const thisPawnSquare: Square = board.findPiece(this);
-        let moveNewRow: number =
-            this.player === Player.WHITE ?
-                thisPawnSquare.row + 1 :
-                thisPawnSquare.row - 1;
-        return [new Square(moveNewRow, thisPawnSquare.col)];
+        const availableMoves: Square[] = [];
+
+        const topRow: number = GameSettings.BOARD_SIZE - 1;
+        const originalStartRow: number = (this.player === Player.WHITE) ? 1 : topRow - 1;
+        const moveRowOffset: number = (this.player === Player.WHITE) ? +1 : -1;
+        
+        const hasNotYetMoved: Boolean = (thisPawnSquare.row === originalStartRow);
+                
+        let moveNewRow: number = thisPawnSquare.row + moveRowOffset;
+        availableMoves.push(new Square(moveNewRow, thisPawnSquare.col));
+        
+        if (hasNotYetMoved) {
+            moveNewRow += moveRowOffset;
+            availableMoves.push(new Square(moveNewRow, thisPawnSquare.col));
+        }
+
+        return availableMoves;
     }
 }
