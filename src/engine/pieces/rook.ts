e96ce98f1db73dce89e3b6944b3e4b39eaf85926
Richard Stuart (Softwire) <richard.stuart@softwire.com>
2026-09-11 17:18:43 +0100

    Claude implemented rook move availability and Red-3 tests pass

--- src/engine/pieces/rook.ts
+++ src/engine/pieces/rook.ts
@@ -1,6 +1,8 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from '../square';
+import GameSettings from '../gameSettings';
 
 export default class Rook extends Piece {
     public constructor(player: Player) {
@@ -8,6 +10,21 @@ export default class Rook extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        const thisRookSquare: Square = board.findPiece(this);
+        const availableMoves: Square[] = [];
+
+        for (let row = 0; row < GameSettings.BOARD_SIZE; row++) {
+            if (row !== thisRookSquare.row) {
+                availableMoves.push(Square.at(row, thisRookSquare.col));
+            }
+        }
+
+        for (let col = 0; col < GameSettings.BOARD_SIZE; col++) {
+            if (col !== thisRookSquare.col) {
+                availableMoves.push(Square.at(thisRookSquare.row, col));
+            }
+        }
+
+        return availableMoves;
     }
 }
